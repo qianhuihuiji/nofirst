7bdbabcc98357979cacbd4d60803a2caf136107f
meiyiming <may.mei@bluepay.asia>
2019-01-04 10:20:28 +0800

    episode 2

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Thread;
 use Illuminate\Http\Request;
 
-class ThreadController extends Controller
+class ThreadsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,7 +14,9 @@ class ThreadController extends Controller
      */
     public function index()
     {
-        //
+        $threads = Thread::latest()->get();
+
+        return view('threads.index',compact('threads'));
     }
 
     /**
@@ -39,14 +41,14 @@ class ThreadController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified thread.
      *
      * @param  \App\Thread  $thread
      * @return \Illuminate\Http\Response
      */
     public function show(Thread $thread)
     {
-        //
+        return view('threads.show',compact('thread'));
     }
 
     /**
