7fb01df985270a180293814827a2bd14dc472aa4
meiyiming <may.mei@bluepay.asia>
2019-01-06 15:41:42 +0800

    episode 10

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -12,6 +12,32 @@ class CreateThreadsTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    public function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel',2)->create();
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+        ->assertSessionHasErrors('channel_id');
+    }
+
+    /** @test */
     public function guests_may_not_create_threads()
     {
         $this->withExceptionHandling();
@@ -28,11 +54,20 @@ class CreateThreadsTest extends TestCase
     {
         $this->signIn();
 
-        $thread = create('App\Thread');
-        $this->post('/threads',$thread->toArray());
+        $thread = make('App\Thread');
+        $response = $this->post('/threads',$thread->toArray());
 
-        $this->get($thread->path())
+        $this->get($response->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);  
+    }
+
+    protected function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread',$overrides );
+
+        return $this->post('/threads',$thread->toArray());
     }
 }
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -10,6 +10,18 @@ class ParticipateInForumTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply',['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
     public function unauthenticated_user_may_not_add_replies()
     {
         $this->withExceptionHandling()
