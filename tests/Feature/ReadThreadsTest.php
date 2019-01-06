9f11ab16766c1dff2da21ccff50675e1f79fa667
meiyiming <may.mei@bluepay.asia>
2019-01-06 21:06:05 +0800

    episode 14

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -40,4 +40,17 @@ class ReadThreadsTest extends TestCase
         $this->get($this->thread->path())
             ->assertSee($reply->body);
     }
+
+    /** @test */
+    public function a_user_can_filter_threads_by_any_username()
+    {
+        $this->signIn(create('App\User',['name' => 'NoNo1']));
+
+        $threadByNoNo1 = create('App\Thread',['user_id' => auth()->id()]);
+        $threadNotByNoNo1 = create('App\Thread');
+
+        $this->get('/threads?by=NoNo1')
+            ->assertSee($threadByNoNo1->title)
+            ->assertDontSee($threadNotByNoNo1->title);
+    }
 }
