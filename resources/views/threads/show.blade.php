e2788d82a0a66f201da774506859ef10b5f53fb4
meiyiming <may.mei@bluepay.asia>
2019-01-07 09:41:06 +0800

    episode 16

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <a href="#">{{ $thread->creator->name }}</a>
@@ -14,21 +14,16 @@
                         {{ $thread->body }}
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                @foreach ($thread->replies as $reply)
+                @foreach($replies as $reply)
                     @include('threads.reply')
                 @endforeach
-            </div>
-        </div>
 
-        @if(auth()->check())
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
+                {{ $replies->links() }}
+
+                @if (auth()->check())
                     <form method="post" action="{{ $thread->path() . '/replies' }}">
+
                         {{ csrf_field() }}
 
                         <div class="form-group">
@@ -37,10 +32,21 @@
 
                         <button type="submit" class="btn btn-default">提交</button>
                     </form>
+                @else
+                    <p class="text-center">请先<a href="{{ route('login') }}">登录</a>，然后再发表回复 </p>
+                @endif
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>
+                            <a href="#">{{ $thread->creator->name }}</a> 发布于 {{ $thread->created_at->diffForHumans() }},
+                            当前共有 {{ $thread->replies_count }} 个回复。
+                        </p>
+                    </div>
                 </div>
             </div>
-            @else
-            <p class="text-center">请先<a href="{{ route('login') }}">登录</a>，然后再发表回复 </p>
-        @endif
+        </div>
     </div>
 @endsection
