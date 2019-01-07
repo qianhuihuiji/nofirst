d8444205db136b962e688eb5aaca5b1a5cef4313
meiyiming <may.mei@bluepay.asia>
2019-01-07 11:05:43 +0800

    episode 19

--- app/Reply.php
+++ app/Reply.php
@@ -10,21 +10,25 @@ class Reply extends Model
 
     public function owner()
     {
-        return $this->belongsTo(User::class,'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function favorites()
     {
-        return $this->morphMany(Favorite::class,'favorited');
+        return $this->morphMany(Favorite::class, 'favorited');
     }
 
     public function favorite()
     {
         $attributes = ['user_id' => auth()->id()];
 
-        if(! $this->favorites()->where($attributes)->exists()){
+        if (! $this->isFavorited()) {
             return $this->favorites()->create($attributes);
         }
-        
+    }
+
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id',auth()->id())->exists();
     }
 }
