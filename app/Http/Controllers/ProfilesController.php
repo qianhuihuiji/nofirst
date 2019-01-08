4e16291eca096e03ece0269b5d82ea20813790c7
meiyiming <may.mei@bluepay.asia>
2019-01-08 13:19:37 +0800

    episode 27

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,23 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Activity;
 
 class ProfilesController extends Controller
 {
     public function show(User $user)
     {
-        
-
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => $this->getActivity($user)
+            'activities' => Activity::feed($user)
         ]);
-    }
-
-    protected function getActivity(User $user)
-    {
-        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
     }
 }
