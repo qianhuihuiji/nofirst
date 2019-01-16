c2cc5959a7f06441e658c363821519a009a6add9
meiyiming <may.mei@bluepay.asia>
2019-01-16 21:36:10 +0800

    episode 62

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -108,7 +108,7 @@ class ThreadsController extends Controller
             $threads->where('channel_id', $channel->id);
         }
 
-        $threads = $threads->get();
+        $threads = $threads->paginate(20);
         return $threads;
     }
 }
---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,29 @@
+@forelse($threads as $thread)
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="level">
+                <h4 class="flex">
+                    <a href="{{ $thread->path() }}">
+                        @if(auth()->check() && $thread->hasUpdatedFor(auth()->user()))
+                            <strong>
+                                {{ $thread->title }}
+                            </strong>
+                        @else
+                            {{ $thread->title }}
+                        @endif
+                    </a>
+                </h4>
+
+                <a href="{{ $thread->path() }}">
+                    {{ $thread->replies_count }} {{ str_plural('reply',$thread->replies_count) }}
+                </a>
+            </div>
+        </div>
+
+        <div class="panel-body">
+                {{ $thread->body }}
+        </div>
+    </div>
+@empty
+    <p>There are no relevant results at this time.</p>
+@endforelse
