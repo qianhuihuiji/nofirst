7e2b939b2c7c74f5db14dc7c568ee399a3ceddab
meiyiming <may.mei@bluepay.asia>
2019-01-16 13:54:37 +0800

    episode 61

--- resources/assets/js/components/NewReply.vue
+++ resources/assets/js/components/NewReply.vue
@@ -19,6 +19,9 @@
 </template>
 
 <script>
+    import 'jquery.caret';
+    import 'at.js';
+    
     export default {
         data() {
             return {
@@ -30,6 +33,20 @@
             signedIn() {
                 return window.App.signIn;
             }
+        },
+
+        mounted() {
+            $('#body').atwho({
+              at:"@",
+              delay:750,
+              callbacks: {
+                  remoteFilter: function (query,callback) {
+                      $.getJSON("/api/users",{name:query},function(usernames){
+                            callback(usernames)
+                          });
+                  }
+              }
+          });
         },
 
         methods: {
