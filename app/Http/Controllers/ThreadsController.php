d52e9d835b8104cc660313615786876b897c84ab
meiyiming <may.mei@bluepay.asia>
2019-01-18 13:30:41 +0800

    episode 66

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Channel;
 use App\Filters\ThreadsFilters;
 use App\Inspections\Spam;
+use Illuminate\Support\Facades\Redis;
 
 class ThreadsController extends Controller
 {
@@ -28,7 +29,9 @@ class ThreadsController extends Controller
             return $threads;
         }
 
-        return view('threads.index', compact('threads'));
+        $trending = array_map('json_decode',Redis::zrevrange('trending_threads',0,4));
+
+        return view('threads.index', compact('threads','trending'));
     }
 
     /**
@@ -77,6 +80,11 @@ class ThreadsController extends Controller
         if(auth()->check()) {
             auth()->user()->read($thread);
         }
+
+        Redis::zincrby('trending_threads',1,json_encode([
+            'title' => $thread->title,
+            'path' => $thread->path()
+        ]));
 
         return view('threads.show', compact('thread'));
     }
