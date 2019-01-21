4c8e47e2e25c40281b6feaef294e487fc7b9050a
meiyiming <may.mei@bluepay.asia>
2019-01-21 23:10:43 +0800

    episdoe 99

---
+++ resources/assets/js/components/Wysiwyg.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input id="trix" type="hidden" :name="name" :value="value">
+
+        <trix-editor ref="trix" input="trix"></trix-editor>
+    </div>
+</template>
+
+<script>
+    // 引入 trix
+    import Trix from 'trix';
+
+    export default {
+        props:['name','value'],
+
+        mounted () {
+            this.$refs.trix.addEventListener('trix-change', e => {
+                this.$emit('input', e.target.innerHTML);
+            })
+        }
+    }
+</script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/trix/0.11.4/trix.css" rel="stylesheet">
 
     <script>
         window.App = {!! json_encode([
