fc4a9a84bd677d012eae026a12528f9927777ec5
meiyiming <may.mei@bluepay.asia>
2019-01-23 20:00:48 +0800

    公共表单验证错误视图

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -44,17 +44,11 @@
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary">Publish</button>
                             </div>
-
-                            @if(count($errors))
-                                <ul class="alert alert-danger">
-                                    @foreach($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            @endif
                         </form>
 
                     </div>
+
+                    @include('common.error')
                 </div>
             </div>
         </div>
