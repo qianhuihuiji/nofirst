ab82b91c7641fc1ee4e304210a585acc3113cbf5
vagrant <vagrant@homestead>
2019-03-11 05:17:03 +0000

    图片上传 ing

---
+++ app/Handlers/ImageUploadHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Handlers;
+
+class ImageUploadHandler
+{
+    protected $allowed_ext = ['png','jpg','gif','jpeg'];
+
+    public function save($file,$folder,$file_prefix)
+    {
+        
+    }
+}
